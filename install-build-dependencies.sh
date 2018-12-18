675eb4722a8f33591ab11448ea62810b77e2af13
Lauri Ojansivu <x@xet7.org>
2018-12-18 22:40:32 +0200

    - Fix: Use command "apt-get" instead of "apt" so it works also on   Ubuntu and Mint.

--- install-build-dependencies.sh
+++ install-build-dependencies.sh
@@ -3,7 +3,7 @@
 # Requires Debian 9 or Ubuntu 16.10 or newer
 
 # Install deb packages
-sudo apt -y install ruby-full rake gperf pkg-config bison libglew-dev \
+sudo apt-get -y install ruby-full rake gperf pkg-config bison libglew-dev \
   freeglut3-dev gettext libpng-dev libpcre3-dev libxml++2.6-dev \
   libfreetype6-dev libdevil-dev libboost-all-dev libsfml-dev \
   libcegui-mk2-dev cmake build-essential git git-core
